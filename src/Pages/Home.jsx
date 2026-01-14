669f5b29596710d15ec50da3f99640384fc0a84b
joseeduardodeoliveiraaraujo-prog <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>
2026-01-14 15:31:39 -0300

    upload funcional

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,18 +1,35 @@
 import Header from "../components/Header/Header";
 import "./Home.css";
 import { FaPlus } from "react-icons/fa";
+import { useRef } from "react";
 
 const Home = ({ user, onLogout }) => {
+  
+  const fileInputRef = useRef(null);
+  
+  const handleUploadClick = () => {
+    fileInputRef.current.click();
+
+  }
   return (
     <>
       <Header user={user} onLogout={onLogout} />
 
       <main className="home-content">
         <h1 className="home-title">Modelo de Certificados</h1>
+
+        <input
+          type="file" 
+          accept="image/*" 
+          ref={fileInputRef} 
+          style={{display:"none"}} 
+          onChange={(e) => console.log(e.target.files[0])}
+        />
         
         <div className="certificates-grid">
           {/*card de upload*/}
-          <div className="certificate-card-upload-card">
+          <div className="certificate-card-upload-card" 
+            onClick={handleUploadClick}>
             <FaPlus/>
             <span>Novo modelo</span>
           </div>
