87567c0368a605b00c203ae526b7760452075f41
joseeduardodeoliveiraaraujo-prog <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>
2026-01-14 14:18:59 -0300

    grids da pagina home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,5 +1,6 @@
 import Header from "../components/Header/Header";
 import "./Home.css";
+import { FaPlus } from "react-icons/fa";
 
 const Home = ({ user, onLogout }) => {
   return (
@@ -7,8 +8,39 @@ const Home = ({ user, onLogout }) => {
       <Header user={user} onLogout={onLogout} />
 
       <main className="home-content">
-        <h1>Conteúdo da página</h1>
-        <p>Aqui fica o restante do site</p>
+        <h1 className="home-title">Modelo de Certificados</h1>
+        
+        <div className="certificates-grid">
+          {/*card de upload*/}
+          <div className="certificate-card-upload-card">
+            <FaPlus/>
+            <span>Novo modelo</span>
+          </div>
+
+          {/*cards de certificados(mock) */}
+          <div className="certificate-card">
+            <img
+              src="https://via.placeholder.com/300x200"
+              alt="certificados"
+            />
+          </div>
+
+          <div className="certificate-card">
+            <img
+              src="https://via.placeholder.com/300x200"
+              alt="Certificados"
+            />
+          </div>
+
+          <div className="certificate-card">
+            <img
+              src="https://via.placeholder.com/300x200"
+              alt="Certificados"
+            />
+          </div>
+
+        </div>
+      
       </main>
     </>
   );
